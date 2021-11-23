a31376356201fa6cd6b82475fe9ece078b6f88de
Caio César Silva <caiocesarsilva08@gmail.com>
2021-11-23 20:40:08 -0300

    Instalação do plugin vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib';
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
-
-Vue.use(Vuetify);
